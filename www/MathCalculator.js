a8aa08477ea40bf0e8e7a8b7daae986a728587bc
Shyam <shyam.pindiproli@orica.com>
2019-07-20 21:26:04 +0800

    Initial commit

---
+++ www/MathCalculator.js
@@ -0,0 +1,29 @@
+var exec = require('cordova/exec');
+
+module.exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
+};
+
+module.exports.add = function (arg0, success, error)
+{
+    exec(success, error, 'MathCalculator', 'add',[arg0])
+
+}
+
+module.exports.subtract = function (arg0, success, error)
+{
+    exec(success, error, 'MathCalculator', 'subtract',[arg0])
+
+}
+
+module.exports.multiply = function (arg0, success, error)
+{
+    exec(success, error, 'MathCalculator', 'subtract',[arg0])
+
+}
+
+module.exports.divide = function (arg0, success, error)
+{
+    exec(success, error, 'MathCalculator', 'subtract',[arg0])
+
+}
